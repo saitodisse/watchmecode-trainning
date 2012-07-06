ff54ca69a06197e0a384e54a81f64d6e6140fa62
Julio Makdisse Saito <saitodisse@gmail.com>
2012-07-05 22:20:42 -0300

    define foo variable

--- 03-JavaScript-Scope/spec/var-scope.spec.js
+++ 03-JavaScript-Scope/spec/var-scope.spec.js
@@ -1,5 +1,12 @@
-describe("variables", function(){
-    it("var", function(){
-        expect(3).toEqual(3);
+describe("variables without 'var' are global", function(){
+    it("without 'var' is global", function(){
+        undefineFoo();
+        defineFoo();
+        expect(callFoo()).toEqual("I'm global!");
+    })
+    it("with 'var' is scoped", function(){
+        undefineFoo();
+        defineFooVar();
+        expect(callFoo()).toEqual(undefined);
     })
 });
--- 03-JavaScript-Scope/src/var-scope.js
+++ 03-JavaScript-Scope/src/var-scope.js
@@ -1,1 +1,15 @@
+function undefineFoo(){
+    foo = undefined;
+}
 
+function defineFoo(){
+    foo = "I'm global!";
+}
+
+function defineFooVar(){
+    var foo = "I'm global!";
+}
+
+function callFoo(){
+    return foo;
+}

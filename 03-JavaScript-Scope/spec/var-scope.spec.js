4d11709d135c30f0f3484b30377d5f3aa6703749
Julio Makdisse Saito <saitodisse@gmail.com>
2012-07-07 00:22:29 -0300

    about scopes

--- 03-JavaScript-Scope/spec/var-scope.spec.js
+++ 03-JavaScript-Scope/spec/var-scope.spec.js
@@ -1,12 +1,42 @@
-describe("variables without 'var' are global", function(){
-    it("without 'var' is global", function(){
-        undefineFoo();
-        defineFoo();
+describe("variables declaration: ", function () {
+    it("without 'var', even inside a function, is global", function () {
+        defineGlobalFoo();
         expect(callFoo()).toEqual("I'm global!");
     })
-    it("with 'var' is scoped", function(){
-        undefineFoo();
-        defineFooVar();
-        expect(callFoo()).toEqual(undefined);
+
+    it("but with 'var', is scoped to the function", function () {
+        deleteGlobalFoo();
+        defineFooWithVar();
+        // does not exist at all
+        expect(window['foo']).toBeUndefined();
+    })
+
+    it("this is how to check not defined variables", function () {
+        // http://stackoverflow.com/questions/858181/how-to-check-a-not-defined-variable-in-javascript
+        expect(typeof thisNotExist).toEqual('undefined');
+        expect(window['thisNotExist']).toBeUndefined();
+    })
+});
+
+describe("hoisting:", function () {
+    it("variables are always moved to the top", function () {
+        hoistedTrueFooVar();
+        expect(hoistedTrueFooVar()).toEqual("fooHoisted");
+    })
+    it("...even if not reached in runtime", function () {
+        hoistedFalseFooVar();
+        expect(hoistedFalseFooVar()).toEqual(undefined);
+    })
+});
+
+describe("closures:", function () {
+    it("without calling inner the var not change", function () {
+        expect(outerScope(false), "outerVar");
+    })
+    it("but calling inner function, change outer variable", function () {
+        expect(outerScope(true), "closure variable");
+    })
+    it("that is not global. Does not exist outside", function () {
+        expect(typeof outerVar).toEqual('undefined');
     })
 });
--- 03-JavaScript-Scope/src/var-scope.js
+++ 03-JavaScript-Scope/src/var-scope.js
@@ -1,15 +1,43 @@
-function undefineFoo(){
-    foo = undefined;
-}
-
-function defineFoo(){
+function defineGlobalFoo(){
     foo = "I'm global!";
-}
-
-function defineFooVar(){
-    var foo = "I'm global!";
 }
 
 function callFoo(){
     return foo;
 }
+
+function deleteGlobalFoo(){
+    delete foo;
+}
+
+function defineFooWithVar(){
+    var foo = "I'm global!";
+}
+
+
+
+function hoistedTrueFooVar(){
+    if(true){
+        var fooHoisted = "fooHoisted";
+    }
+    return fooHoisted;
+}
+function hoistedFalseFooVar(){
+    if(false){
+        var fooHoisted = "fooHoisted";
+    }
+    return fooHoisted;
+}
+
+
+function outerScope(mustCall){
+    var outerVar = "outerVar";
+    function innerScope(){
+        outerVar = "closure variable";
+    }
+    if(mustCall){
+        innerScope();
+    }
+    return outerVar;
+}
+

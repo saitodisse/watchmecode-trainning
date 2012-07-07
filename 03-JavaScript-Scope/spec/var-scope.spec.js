ff0c12a98be8b802f8376751a16acbe644f4fa78
Julio Makdisse Saito <saitodisse@gmail.com>
2012-07-07 00:43:04 -0300

    all in the spec file

--- 03-JavaScript-Scope/spec/var-scope.spec.js
+++ 03-JavaScript-Scope/spec/var-scope.spec.js
@@ -1,12 +1,22 @@
 describe("variables declaration: ", function () {
     it("without 'var', even inside a function, is global", function () {
+        function defineGlobalFoo(){
+            foo = "I'm global!";
+        }
+
+        function callFoo(){
+            return foo;
+        }
+
         defineGlobalFoo();
         expect(callFoo()).toEqual("I'm global!");
     })
 
     it("but with 'var', is scoped to the function", function () {
-        deleteGlobalFoo();
-        defineFooWithVar();
+        delete foo;
+        function defineFooWithVar(){
+            var foo = "I'm global!";
+        }
         // does not exist at all
         expect(window['foo']).toBeUndefined();
     })
@@ -20,16 +30,38 @@ describe("variables declaration: ", function () {
 
 describe("hoisting:", function () {
     it("variables are always moved to the top", function () {
-        hoistedTrueFooVar();
+        function hoistedTrueFooVar(){
+            if(true){
+                var fooHoisted = "fooHoisted";
+            }
+            return fooHoisted;
+        }
         expect(hoistedTrueFooVar()).toEqual("fooHoisted");
     })
     it("...even if not reached in runtime", function () {
-        hoistedFalseFooVar();
+        function hoistedFalseFooVar(){
+            if(false){
+                var fooHoisted = "fooHoisted";
+            }
+            return fooHoisted;
+        }
         expect(hoistedFalseFooVar()).toEqual(undefined);
     })
 });
 
 describe("closures:", function () {
+
+    function outerScope(mustCall){
+        var outerVar = "outerVar";
+        function innerScope(){
+            outerVar = "closure variable";
+        }
+        if(mustCall){
+            innerScope();
+        }
+        return outerVar;
+    }
+
     it("without calling inner the var not change", function () {
         expect(outerScope(false), "outerVar");
     })
@@ -38,5 +70,22 @@ describe("closures:", function () {
     })
     it("that is not global. Does not exist outside", function () {
         expect(typeof outerVar).toEqual('undefined');
+    })
+});
+
+
+describe("immediate functions:", function () {
+    it("you can point your function to a variable", function () {
+        var myFunc = function(){
+            var a = 1;
+            expect(a).toEqual(1);
+        };
+        myFunc();
+    })
+    it("but, to be immediate you have to call it", function () {
+        (function(){
+            var a = 1;
+            expect(a).toEqual(1);
+        })();
     })
 });
